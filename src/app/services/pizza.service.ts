8493ab97afcabaa3cc4414d6fd38c983c0209411
Lucas Monfort <Lucas_Monfort@etu.ube.fr>
2026-04-15 10:38:49 +0200

    Ajout de 3 pizza et du getter associée au service des pizza

--- src/app/services/pizza.service.ts
+++ src/app/services/pizza.service.ts
@@ -5,5 +5,15 @@ import { Pizza } from '../models/pizza'
   providedIn: 'root',
 })
 export class PizzaService {
-  
+
+  pizzas: Pizza[] = [
+    { id: 1, name: 'Margherita', price: 8, image: "assets/margherita.png" },
+    { id: 2, name: 'Pepperoni', price: 10, image: "assets/pepperoni.png" },
+    { id: 3, name: '4 Fromages', price: 12, image: "assets/fromages.png" }
+  ];
+
+  getPizzas(): Pizza[] {
+    return this.pizzas
+  }
+
 }
