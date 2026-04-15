bb620d07b6b6471b6c81d3d7c7844c62f25ed149
Lucas Monfort <Lucas_Monfort@etu.ube.fr>
2026-04-15 11:37:45 +0200

    Visuel meilleur

--- src/app/services/pizza.service.ts
+++ src/app/services/pizza.service.ts
@@ -7,9 +7,9 @@ import { Pizza } from '../models/pizza'
 export class PizzaService {
 
   pizzas: Pizza[] = [
-    { id: 1, name: 'Margherita', price: 8, image: "assets/margherita.png" },
-    { id: 2, name: 'Pepperoni', price: 10, image: "assets/pepperoni.png" },
-    { id: 3, name: '4 Fromages', price: 12, image: "assets/fromages.png" }
+    { id: 1, name: 'Margherita', price: 8, image: "/assets/margherita.jpg" },
+    { id: 2, name: 'Pepperoni', price: 10, image: "/assets/pepperoni.jpg" },
+    { id: 3, name: '4 Fromages', price: 12, image: "/assets/fromages.jpg" }
   ];
 
   getPizzas(): Pizza[] {
