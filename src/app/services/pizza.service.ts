5ab9ca45107cde5d8eec27431173cc77a966a1f1
Lucas Monfort <Lucas_Monfort@etu.ube.fr>
2026-04-15 10:29:34 +0200

    renommage des fichiers de ses morts

--- src/app/services/pizza.service.ts
+++ src/app/services/pizza.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Pizza } from '../models/pizza'
 
 @Injectable({
   providedIn: 'root',
 })
-export class Pizza {}
+export class PizzaService {
+  
+}
